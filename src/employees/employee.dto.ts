e17f084c6bbee70c613e3c29184160ecef4f04cb
Paola Andrea Cardona Salcedo <pao_cardona97@hotmail.com>
2020-10-24 13:23:12 -0500

    employee and role finished

---
+++ src/employees/employee.dto.ts
@@ -0,0 +1,9 @@
+export class EmployeeDTO {
+    name: string;
+    password: string;
+    email: string;
+    cellphone: number;
+    registryDate: Date;
+    removalDate: Date;
+    roleId: number;
+}
