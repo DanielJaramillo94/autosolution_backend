614eb6cf82433a32cd5a113916fa100388afa5a6
Daniel <daniel.jaramillo7@udea.edu.co>
2021-01-17 19:30:44 -0500

    feat: authentication working as expected

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -9,7 +9,6 @@ export class AuthService {
   constructor(private employeesService: EmployeesService, private jwtService: JwtService) {}
 
   async validateUser(email: string, pass: string): Promise<any> {
-
     const employee = await this.employeesService.findByEmail(email);
     if (employee && compareSync(pass, employee.password)) {
       const { password, ...result } = employee;
@@ -19,7 +18,7 @@ export class AuthService {
   }
 
   async login(user: any) {
-    const payload = { username: user.username, sub: user.userId };
+    const payload = { username: user.email, sub: user.id };
     return {
       // eslint-disable-next-line @typescript-eslint/camelcase
       access_token: this.jwtService.sign(payload),
