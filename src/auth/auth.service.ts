6c97198f72bac0fb1c378807692825115c5d26b4
Paola Andrea Cardona Salcedo <pao_cardona97@hotmail.com>
2021-01-25 02:29:43 -0500

    send email access to the owner

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { EmployeesService } from '../employees/employees.service';
 import { OwnersService } from '../owners/owners.service';
+import * as nodemailer from 'nodemailer';
 
 
 @Injectable()
@@ -35,13 +36,41 @@ export class AuthService {
     return null;
   }
 
-  async createToken(user: any) {
-    const payload = { name: user.name, id: user.id };    
-    return { 
-      access_token: this.jwtService.sign(payload,{
-        expiresIn: '30s'
-      },),
-    };       
+  async sendEmail(email: string, link: string) {
+    const transporter = nodemailer.createTransport({
+      host: "smtp.sendgrid.net",
+      port: 465,
+      secure: true,
+      auth: {
+        user: 'apikey',
+        pass: process.env.SENDGRID_API_KEY,
+      },
+    });
+
+    // send mail with defined transport object
+    const info = await transporter.sendMail({
+      from: '"AutoSolution" <pao_cardona97@hotmail.com>', // sender address
+      to: email, // list of receivers
+      subject: "Enlace de acceso", // Subject line
+      text: "Enlace de acceso", // plain text body
+      html: `<h4>Por favor haga clic en el siguiente enlace:</h4> <a href="${link}">Aquí</a>`, // html body
+    });
   }
 
+  async createToken(user: any) {
+    const payload = { name: user.name, id: user.id };
+    // eslint-disable-next-line @typescript-eslint/camelcase
+    const data = {
+      // eslint-disable-next-line @typescript-eslint/camelcase
+      access_token: this.jwtService.sign(payload, {
+        expiresIn: '30s'
+      }),
+    };
+    const token = data.access_token;
+    const link = `${process.env.FRONTEND_HOST}/owner?id=${user.id}&token=${token}`;
+    this.sendEmail(user.email, link);
+    return {
+      token
+    };
+  }
 }
