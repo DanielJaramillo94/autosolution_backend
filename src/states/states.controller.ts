09284eaf6c36ca7fe2a983ee3da0debdcf7292de
Santiago Quintero <santiago.quinteroh@udea.edu.co>
2021-01-25 20:38:55 -0500

    Corrección roles

--- src/states/states.controller.ts
+++ src/states/states.controller.ts
@@ -1,4 +1,13 @@
-import { Controller, Get, Param, Post, Put, Delete, Body, UseGuards } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Param,
+  Post,
+  Put,
+  Delete,
+  Body,
+  UseGuards,
+} from '@nestjs/common';
 import { StatesService } from './states.service';
 import { StateDTO } from './state.dto';
 import { Roles } from 'src/roles/roles.decorator';
@@ -7,40 +16,40 @@ import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 import { RolesGuard } from 'src/roles/roles.guard';
 @Controller('states')
 export class StatesController {
-    constructor (private statesService: StatesService) {}
+  constructor(private statesService: StatesService) {}
 
-    @Get()
-    @UseGuards(JwtAuthGuard, RolesGuard)
-    @Roles(Role.ManagementAssistant)
-    findAll() {
-        return this.statesService.findAll();
-    }
+  @Get()
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(Role.ManagementAssistant, Role.Supervisor, Role.Mechanic)
+  findAll() {
+    return this.statesService.findAll();
+  }
 
-    @Get(':id')
-    @UseGuards(JwtAuthGuard, RolesGuard)
-    @Roles(Role.ManagementAssistant)
-    async findById(@Param('id') stateId: number) {
-        return await this.statesService.findById(stateId);
-    }
+  @Get(':id')
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(Role.ManagementAssistant, Role.Supervisor, Role.Mechanic)
+  async findById(@Param('id') stateId: number) {
+    return await this.statesService.findById(stateId);
+  }
 
-    @Post()
-    @UseGuards(JwtAuthGuard, RolesGuard)
-    @Roles(Role.ManagementAssistant)
-    async create(@Body() newState: StateDTO){
-        return this.statesService.create(newState);
-    }
+  @Post()
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(Role.ManagementAssistant)
+  async create(@Body() newState: StateDTO) {
+    return this.statesService.create(newState);
+  }
 
-    @Put(':id')
-    @UseGuards(JwtAuthGuard, RolesGuard)
-    @Roles(Role.ManagementAssistant)
-    async replace(@Param('id') stateId: number, @Body() newState: StateDTO) {
-        return this.statesService.replace(stateId, newState);
-    }
+  @Put(':id')
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(Role.ManagementAssistant)
+  async replace(@Param('id') stateId: number, @Body() newState: StateDTO) {
+    return this.statesService.replace(stateId, newState);
+  }
 
-    @Delete(':id')
-    @UseGuards(JwtAuthGuard, RolesGuard)
-    @Roles(Role.ManagementAssistant)
-    async delete(@Param('id') stateId) {
-        return this.statesService.delete(stateId);
-    }
+  @Delete(':id')
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(Role.ManagementAssistant)
+  async delete(@Param('id') stateId) {
+    return this.statesService.delete(stateId);
+  }
 }
--- src/vehiclesXowners/vehiclesXowners.controller.ts
+++ src/vehiclesXowners/vehiclesXowners.controller.ts
@@ -1,4 +1,13 @@
-import { Controller, Get, Param, Post, Put, Delete, Body, UseGuards } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Param,
+  Post,
+  Put,
+  Delete,
+  Body,
+  UseGuards,
+} from '@nestjs/common';
 import { VehicleXownersService } from './vehiclesXowners.service';
 import { VehicleXownerDTO } from './vehicleXowner.dto';
 import { Roles } from 'src/roles/roles.decorator';
@@ -7,47 +16,53 @@ import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 import { RolesGuard } from 'src/roles/roles.guard';
 @Controller('vehicleXowners')
 export class VehicleXownersController {
-    constructor(private vehicleXownersService: VehicleXownersService) { }
+  constructor(private vehicleXownersService: VehicleXownersService) {}
 
-    @Get()
-    @UseGuards(JwtAuthGuard, RolesGuard)
-    @Roles(Role.ManagementAssistant)
-    findAll() {
-        return this.vehicleXownersService.findAll();
-    }
+  @Get()
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(Role.ManagementAssistant)
+  findAll() {
+    return this.vehicleXownersService.findAll();
+  }
 
-    @Get(':id')
-    @UseGuards(JwtAuthGuard, RolesGuard)
-    @Roles(Role.ManagementAssistant)
-    async findById(@Param('id') vehicleXownerId: number) {
-        return await this.vehicleXownersService.findById(vehicleXownerId);
-    }
+  @Get(':id')
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(Role.ManagementAssistant)
+  async findById(@Param('id') vehicleXownerId: number) {
+    return await this.vehicleXownersService.findById(vehicleXownerId);
+  }
 
-    @Get('owner/:vehicleId')
-    @UseGuards(JwtAuthGuard, RolesGuard)
-    @Roles(Role.ManagementAssistant)
-    async findOwnerByVehicleId(@Param('vehicleId') vehicleId: number) {
-        return await this.vehicleXownersService.findOwnerByVehicleId(vehicleId);
-    }
+  @Get('owner/:vehicleId')
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  //   @Roles(Role.ManagementAssistant, Role.Supervisor)
+  async findOwnerByVehicleId(@Param('vehicleId') vehicleId: number) {
+    return await this.vehicleXownersService.findOwnerByVehicleId(vehicleId);
+  }
 
-    @Post()
-    @UseGuards(JwtAuthGuard, RolesGuard)
-    @Roles(Role.ManagementAssistant)
-    async create(@Body() newVehicleXowner: VehicleXownerDTO) {
-        return this.vehicleXownersService.create(newVehicleXowner);
-    }
+  @Post()
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(Role.Supervisor)
+  async create(@Body() newVehicleXowner: VehicleXownerDTO) {
+    return this.vehicleXownersService.create(newVehicleXowner);
+  }
 
-    @Put(':id')
-    @UseGuards(JwtAuthGuard, RolesGuard)
-    @Roles(Role.ManagementAssistant)
-    async replace(@Param('id') vehicleXownerId: number, @Body() newVehicleXowner: VehicleXownerDTO) {
-        return this.vehicleXownersService.replace(vehicleXownerId, newVehicleXowner);
-    }
+  @Put(':id')
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(Role.ManagementAssistant)
+  async replace(
+    @Param('id') vehicleXownerId: number,
+    @Body() newVehicleXowner: VehicleXownerDTO,
+  ) {
+    return this.vehicleXownersService.replace(
+      vehicleXownerId,
+      newVehicleXowner,
+    );
+  }
 
-    @Delete(':id')
-    @UseGuards(JwtAuthGuard, RolesGuard)
-    @Roles(Role.ManagementAssistant)
-    async delete(@Param('id') vehicleXownerId) {
-        return this.vehicleXownersService.delete(vehicleXownerId);
-    }
+  @Delete(':id')
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(Role.ManagementAssistant)
+  async delete(@Param('id') vehicleXownerId) {
+    return this.vehicleXownersService.delete(vehicleXownerId);
+  }
 }
